d154b3fa791c9fb952310c38b593764393ddd5ad
sumon-hossain <sumonengr.96@gmail.com>
2022-12-22 14:19:07 +0600

    Redux-Toolkit-CRUD

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddUser from "./features/users/AddUser";
+import EditUser from "./features/users/EditUser";
+import UserList from "./features/users/UserList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
+        <h1 className="text-center font-bold text-2xl text-gray-700">
+          CRUD with redux toolkit
+        </h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<UserList />} />
+            <Route path="/add-user" element={<AddUser />} />
+            <Route path="/edit-user/:id" element={<EditUser />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
